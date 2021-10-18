83d1b2409a010e6dfffbe1e458b4f9d78a190cc5
Matthew D Elmer <matthewelmer@tamu.edu>
2021-10-18 08:46:31 -0500

    Added indentation

--- Wind Tunnel/Expansion.m
+++ Wind Tunnel/Expansion.m
@@ -1,15 +1,15 @@
 function [ xList yList ] = Expansion(r, theta, n)
-rangeAngles = linspace(3*pi/2, 3*pi/2 + theta, n);
-xList = [];
-yList = [];
-for i = 1:length(rangeAngles)
-    x = r*cos(rangeAngles(i));
-    y = r*sin(rangeAngles(i)) + (1+r);
-    xList(i) = x;
-    yList(i) = y;
-plot(xList, yList, '-');
-end
-axis equal
+    rangeAngles = linspace(3*pi/2, 3*pi/2 + theta, n);
+    xList = [];
+    yList = [];
+    for i = 1:length(rangeAngles)
+        x = r*cos(rangeAngles(i));
+        y = r*sin(rangeAngles(i)) + (1+r);
+        xList(i) = x;
+        yList(i) = y;
+        plot(xList, yList, '-');
+    end
+    axis equal
 end
 
     
