fc151823d1a9a3ed3a41cf435743be48445e4547
Elmer <matthewelmer@tamu.edu>
2021-10-26 02:17:24 -0500

    Quick Push

---
+++ Gas Compression/pressure_solving.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# constants
+gamma_air = 1.4
+gamma_mono = 1.67
+R_universal = 8.31446261815324  # J / (mol K)
+molar_mass_air = 0.0289645  # kg / mol
+R_air = R_universal / molar_mass_air  # J / (kg K)
+print(R_air)
+
+
+# Start with test section conditions
+
+# Assume some level of vaccum
+
+# Solve for reservoir conditions
